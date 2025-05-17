e228c0c8d7301ecf6e864069cfdd48172d82f7f2
MacariusIbne <macariusibnemito@gmail.com>
2025-05-17 17:59:49 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeColor(color) {
+  document.getElementById("colorBox").style.backgroundColor = color;
+} // so by pressing it, the larger button will turn into the colour that was pressed or selected on
+// so like if you press red, the larger box will turn red
